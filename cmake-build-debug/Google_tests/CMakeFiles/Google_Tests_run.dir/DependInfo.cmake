94ff5fe483c85149217e3d679c5a6e6c6f6e243e
dmitrijgulacenkov <pingvin1959@yandex.ru>
2019-12-07 13:43:38 +0300

    Относительные пути

---
+++ cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/DependInfo.cmake
@@ -0,0 +1,58 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/Google_tests/API_tests.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/API_tests.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/API/FacebookAPI.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/API/FacebookAPI.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/API/OkAPI.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/API/OkAPI.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/API/VkAPI.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/API/VkAPI.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/Crowler/Crowler.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/Crowler/Crowler.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/Id_list_generator_strategy/Facebook_id_list_generator_strategy.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/Id_list_generator_strategy/Facebook_id_list_generator_strategy.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/Id_list_generator_strategy/Ok_id_list_generator_strategy.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/Id_list_generator_strategy/Ok_id_list_generator_strategy.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/Id_list_generator_strategy/Vk_id_list_generator_strategy.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/Id_list_generator_strategy/Vk_id_list_generator_strategy.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/NetService/NetService.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/NetService/NetService.cpp.o"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/response/response.cpp" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/CMakeFiles/Google_Tests_run.dir/__/response/response.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../AbstractClasses"
+  "../API"
+  "../Crowler"
+  "../Id_list_generator_strategy"
+  "../NetService"
+  "../response"
+  "../url"
+  "../API/FacebookAPI.cpp"
+  "../API/OkAPI.cpp"
+  "../API/VkAPI.cpp"
+  "../Id_list_generator_strategy/Facebook_id_list_generator_strategy.cpp"
+  "../Id_list_generator_strategy/Vk_id_list_generator_strategy.cpp"
+  "../Id_list_generator_strategy/Ok_id_list_generator_strategy.cpp"
+  "../NetService/NetService.cpp"
+  "../response/response.cpp"
+  "../Crowler/Crowler.cpp"
+  "../Google_tests/lib/googletest/include"
+  "../Google_tests/lib/googletest"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/lib/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/Google_tests/lib/googletest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/gtests/gtest_lib/googlemock/CMakeFiles/gmock.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/gtests/gtest_lib/googlemock/src/gmock-all.cc" "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/gtests/gtest_lib/googlemock/CMakeFiles/gmock.dir/src/gmock-all.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../gtests/gtest_lib/googlemock/include"
+  "../gtests/gtest_lib/googlemock"
+  "../gtests/gtest_lib/googletest/include"
+  "../gtests/gtest_lib/googletest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/dmitrijgulacenkov/CLionProjects/kursach_final/cmake-build-debug/gtests/gtest_lib/googletest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
